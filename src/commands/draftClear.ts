211f5a01782c2a326b1301e9d179106cb0bab243
TomoTom0 <tomoIris427+GitHub@gmail.com>
2025-11-21 08:29:04 +0900

    Polish: route draftClear status to stderr and minor cleanups

--- src/commands/draftClear.ts
+++ src/commands/draftClear.ts
@@ -2,5 +2,7 @@ import { clearDrafts } from '../lib/store.js';
 
 export default async function draftClear(prNumber: string) {
   await clearDrafts(prNumber);
-  console.log('Drafts cleared.');
+  // status messages go to stderr to avoid polluting JSON stdout
+  // eslint-disable-next-line no-console
+  console.error('Drafts cleared.');
 }
